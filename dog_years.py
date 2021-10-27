255e41fd7f4cb7018723a04986a5a3b69cb73c87
DannyRong <dannyrong@gmail.com>
2021-10-27 09:47:01 -0400

    Add files via upload

---
+++ dog_years.py
@@ -0,0 +1,24 @@
+def dog_years (dog_calendar):
+    """"compute the human age of dog years
+    
+    Args:
+        dog_calendar (int): age of dog in calendar years
+
+    Raises:
+        ValueError: specify the age when it is negative
+
+    Returns:
+        int: age of the dog in human years
+    """
+
+    human_years = 0
+    if dog_calendar == 1: 
+        human_years = 15
+    elif dog_calendar == 2:
+        human_years = 24
+    elif dog_calendar > 2:
+        human_years = 24 + (dog_calendar-2) * 5
+    return human_years
+
+if __name__ == "__main__":
+    print(dog_years(4))
