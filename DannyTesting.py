d04c9466f2bdd7b80c5866742ac62762ccce4b7d
Danny <dannyrong@gmail.com>
2021-11-14 17:45:19 -0500

    played with testing some salt/hash stuff

--- DannyTesting.py
+++ DannyTesting.py
@@ -4,46 +4,6 @@ import uuid
 from argparse import ArgumentParser
 import sys
 
-class Catalog():
-    
-    def __init__(self, filePath):
-        """ Initializes the catalog
-
-        Args:
-            filePath (string): path to file to be read
-        """
-        self.catalog = {}
-
-        f = open(filePath, 'r')
-        reader = f.readline()
-        while(reader):
-            listOfStuff = reader.strip().split(",")
-            print(listOfStuff)
-
-            reader = f.readline()
-        f.close()
-    
-
-    def get_balance(self, name):
-        """ returns the user's balance
-        
-        Args:
-            name: the user's username
-        """
-        return 0
-
-
-    def add_user(self, user):
-        """ adds a user to the catalog
-        
-        Args:
-            user (user class): an instance of the user class
-
-        """
-
-        return
-
-
 #_____________________testing stuff underhere_______________
 
 
