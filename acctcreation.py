ef7c70f788067ce98570ff212a1e2706103d1eea
Danny <dannyrong@gmail.com>
2021-12-12 18:26:39 -0500

    added salt and hashing to startup

--- acctcreation.py
+++ acctcreation.py
@@ -1,5 +1,8 @@
 import sys
 from argparse import ArgumentParser
+import hashlib
+import base64
+import uuid
 #import password
 
 class acctcreation():
@@ -27,7 +30,13 @@ class acctcreation():
         with open(filepath, 'r', encoding = "utf-8") as new_file:
             for line in new_file:
                 user = line.strip().split(",")
-                self.userinfo[user[0]] = (user[1], float(user[2])) #key:username  tuple: password, money in account
+
+                #salt and hash password here
+                salt = uuid.uuid4().hex
+                hashedpass = hashlib.sha256(user[1].encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+                self.userinfo[user[0]] = (salt, hashedpass, float(user[2])) #key:username  tuple: salt, hashedpass, money
+
     
     
     def create_acct(self):
@@ -40,7 +49,7 @@ class acctcreation():
         created_account = False
         while (created_account is False):
             username = input("Enter your desired username:")
-            pass1 = input("Enter your desired password\nPassword must have a special character, a number, and a capital number:")
+            pass1 = input("Enter your desired password\nPassword must have a special character, a number, and a capital number:\n")
             pass2 = input("Confirm your desired password:")
             if pass1 == pass2:
                 password = pass1
@@ -52,7 +61,8 @@ class acctcreation():
 
                     deposit = input("Please deposit any initial funds or put 0 if you so choose:")
 
-                    #salt and hash password here and then store it to the database with the funds
+                    # TODO salt and hash password here and then store it to the database with the funds
+
                 
                 else:
                     print("Password not secure, generating secure password")
@@ -99,6 +109,24 @@ class acctcreation():
     def take_money(self,user, money):
         print("subtracting money")
 
+    def get_balance(self, name):
+        """ returns the user's balance
+        
+        Args:
+            name: the user's username
+        """
+        return 0
+
+
+    def add_user(self, user):
+        """ adds a user to the catalog
+        
+        Args:
+            user (user class): an instance of the user class
+
+        """
+
+        return
 
 
 
